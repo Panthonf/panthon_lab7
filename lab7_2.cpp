d5378418ab64a4663a47ec8d91d5cebe262248a4
Panthonf <76035497+Panthonf@users.noreply.github.com>
2020-12-30 12:13:24 +0700

    Update lab7_2.cpp

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
 char findGrade(double x){
     if(x >90)
         return 'A';
@@ -10,4 +15,35 @@ char findGrade(double x){
     else
         return 'F';
     
+}
+
+int main(){
+ int N,i = 0;
+ cout << "Enter the number of students: ";
+ cin >> N;
+ string name[N];
+ double score[N]; 
+ 
+ 
+ while(i < N){
+  cout << "Name of student " << i+1 << ": ";
+  cin.ignore();
+  getline(cin,name[i]);
+  cout << "Score of student " << i+1 << ": ";
+  cin >> score[i];
+  i++;
+ }
+ 
+ 
+ i = 0;
+ cout << "---------------------------------------------\n";
+ cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
+ cout << "---------------------------------------------\n";
+ while(i < N){
+  cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
+  i++;
+ } 
+ cout << "---------------------------------------------\n";
+
+ return 0;
 }
